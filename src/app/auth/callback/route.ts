6a8f944e8a6c56a978b4953fe4328af7cc9e0ef7
Vinayak <vinayak@quokkalabs.com>
2025-06-12 11:32:14 +0530

    (fix):Added console to check the logs on server.

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -39,6 +39,8 @@ import { NextResponse } from "next/server";
 import createClient from "@/lib/supabase/server";
 
 export async function GET(request: Request) {
+
+  console.log("request",request);
   const { searchParams, origin } = new URL(request.url);
 
   // Extract auth code and optional redirect path
